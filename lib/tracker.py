b65af21358d382ae8bd0f7b775a67a7c2cbeedb4
jon <jonnyburdis@hotmail.com>
2025-06-11 16:11:18 +0100

    test drive most of birthday tracker

--- lib/tracker.py
+++ lib/tracker.py
@@ -1,10 +1,9 @@
+from datetime import datetime, timedelta
+
 class BirthdayTracker:
 
     def __init__(self):
         self.tracker = {}
-        #no parameters
-        #side effect: creating dict
-        pass
 
     def add_birthday(self, name, birthday):
         #parameters:
@@ -14,7 +13,7 @@ class BirthdayTracker:
         #returns: nothing 
         #side effect: appends entry to dict 
         #calculate age of the user and save it to age property 
-        pass
+        self.tracker[name] = birthday
 
     def edit_birthday(self, name, birthday):
         #parameters:
@@ -24,7 +23,11 @@ class BirthdayTracker:
         #returns: nothing 
         #side effect: checks name exists and updates entry to dict
         #recalculate age based on new date 
-        pass
+        if name in self.tracker.keys():
+            self.tracker[name] = birthday
+        else:
+            raise Exception("No exisiting entry to update")
+
 
     def edit_name(self, old_name, new_name):
         #parameters:
@@ -32,16 +35,22 @@ class BirthdayTracker:
 
         #returns: nothing 
         #side effect: checks name exists and updates the name in dict
+        if old_name in self.tracker.keys():
+            self.tracker[new_name] = self.tracker[old_name]
+            del self.tracker[old_name]
+        else:
+            raise Exception("No entry found")
 
-        pass
 
     def upcoming_birthdays(self):
-        #assume upcoming in next 30 days
-        #no parameters 
-        #returns: 
-            #list of friends with birthdays in next 30 days 
-        #no sideeffects 
-        pass 
+        today = datetime.now()
+        thirty_days_future = today + timedelta(days=30)
+        next_thirty_days = dict(filter(lambda friend: 
+                                    datetime.strptime(friend[1], "%Y-%m-%d") <= thirty_days_future 
+                                    and datetime.strptime(friend[1], "%Y-%m-%d") >= today, 
+                                    self.tracker.items()
+                                    ))
+        return next_thirty_days
 
     def calculate_age(self, name):
         #parameter:
--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,38 +1,70 @@
+import pytest
+from lib.tracker import BirthdayTracker
 
 """
 Given a tracker is created 
 An empty dictonary is created 
 """
-#birthday_tracker = BirthdayTracker()
-#birthday_tracker.tracker == {}
+def test_tracker_is_created_as_empty_dictionary():
+    birthday_tracker = BirthdayTracker()
+    assert birthday_tracker.tracker == {}
 
 """
 Given we add an entry to the dictonary 
 The entry can be accessed in the dictionary
 """
-#birthday_tracker = BirthdayTracker()
-#birthday_tracker.add('name', 'birthday') ==> birthday_tracker.tracker == {'name': 'birthday'}
+def test_adding_entry_to_dictionary_and_access():
+    birthday_tracker = BirthdayTracker()
+    birthday_tracker.add_birthday('Jim', '2020-06-11') 
+    assert birthday_tracker.tracker == {'Jim': '2020-06-11'}
+    # ==> birthday_tracker.tracker == {'name': 'birthday'}
 
 """
 Given we edit a birthday 
 The new birthday is updated in the dictionary 
 """
-#birthday_tracker = BirthdayTracker()
-#birthday_tracker.update_birthday('name', 'birthday') ==> birthday_tracker.tracker == {'name': 'birthday'}
+def test_editing_birthday_date():
+    birthday_tracker = BirthdayTracker()
+    birthday_tracker.add_birthday('Jim', '2020-06-11') 
+    assert birthday_tracker.tracker == {'Jim': '2020-06-11'}
+    birthday_tracker.edit_birthday('Jim', '2019-06-11')
+    assert birthday_tracker.tracker == {'Jim': '2019-06-11'}
+
+
+def test_editing_birthday_date_no_existing_entry():
+    birthday_tracker = BirthdayTracker()
+    with pytest.raises(Exception) as err:
+        birthday_tracker.edit_birthday('Jim', '2019-06-11')
+    err_message = "No exisiting entry to update"
+    assert str(err.value) == err_message
 
 """
 Given we edit the name passing in old name and new name 
 The old name is searched and updated to the new name 
 """
-#birthday_tracker = BirthdayTracker()
-#birthday_tracker.update_name('old_name', 'new_name') ==> birthday_tracker.tracker == {'new_name': 'birthday'}
+def test_editing_name_existing_entry():
+    birthday_tracker = BirthdayTracker()
+    birthday_tracker.add_birthday('Jim', '2020-06-11') 
+    birthday_tracker.edit_name('Jim', 'Jam') 
+    assert birthday_tracker.tracker == {'Jam': '2020-06-11'}
+
+def test_editing_name_no_existing_entry():
+    birthday_tracker = BirthdayTracker()
+    birthday_tracker.add_birthday('Jim', '2020-06-11') 
+    with pytest.raises(Exception) as err:
+        birthday_tracker.edit_name('Bob', 'Bill') 
+    err_message = "No entry found"
+    assert str(err.value) == err_message
 
 """
 Given we call the method of upcoming birthdays 
 We see a list of names and birthdays in the nexy 30 days
 """
-#birthday_tracker = BirthdayTracker()
-#birthday_tracker.upcoming_birthdays() ==> {'name':'birthday'}
+def test_show_birthdays_next_30_days():
+    birthday_tracker = BirthdayTracker()
+    birthday_tracker.add_birthday('Jim', '2025-06-20') 
+    birthday_tracker.add_birthday('Bob', '2025-07-20') 
+    assert birthday_tracker.upcoming_birthdays() == {'Jim': '2025-06-20'}
 
 """
 Given we want to calculate the upcoming ages of friend with birthdays in next 30 days 
